ba2fe60c691d980ee80b67cd0dabd58d2d0f5852
nickywhiff <n.whiffin@imperial.ac.uk>
2017-01-31 15:31:50 +0000

    Adding single stacked bar figure option

--- R/Figures.R
+++ R/Figures.R
@@ -134,3 +134,64 @@ grid.arrange(hcm_no_leg, dcm, legend,
              layout_matrix=rbind(c(1), c(2), c(3)), 
              heights=c(10, 10, 1))
 dev.off()
+
+# Try single stacked bar
+
+data2<-read.table("../data/InternalDataProportions_core_cumulative.txt", header=TRUE, sep="\t")
+data2$Class <- factor(data2$Class, c("Pathogenic", "Likely Pathogenic", "VUS", "Likely Benign", "Benign"))
+
+hcm_hvol_sub<-subset(data2, Test=='HCM' & Cohort=='HVOL' & Class=='VUS')
+hcm_vus_in_hvol<-hcm_hvol_sub$Proportion
+hcm_ben_sub<-subset(data2, Test=='HCM' & Cohort=='HCM' & Class=='Benign')
+hcm_ben<-hcm_ben_sub$Proportion
+hcm_lb_sub<-subset(data2, Test=='HCM' & Cohort=='HCM' & Class=='Likely Benign')
+hcm_lb<-hcm_lb_sub$Proportion
+hcm_path_sub<-subset(data2, Test=='HCM' & Cohort=='HCM' & Class=='Pathogenic')
+hcm_path<-hcm_path_sub$Proportion
+hcm_lp_sub<-subset(data2, Test=='HCM' & Cohort=='HCM' & Class=='Likely Pathogenic')
+hcm_lp<-hcm_lp_sub$Proportion
+
+hcm_line = 1-(hcm_ben+hcm_lb+hcm_vus_in_hvol)
+
+dcm_hvol_sub<-subset(data2, Test=='DCM' & Cohort=='HVOL' & Class=='VUS')
+dcm_vus_in_hvol<-dcm_hvol_sub$Proportion
+dcm_ben_sub<-subset(data2, Test=='DCM' & Cohort=='DCM' & Class=='Benign')
+dcm_ben<-dcm_ben_sub$Proportion
+dcm_lb_sub<-subset(data2, Test=='DCM' & Cohort=='DCM' & Class=='Likely Benign')
+dcm_lb<-dcm_lb_sub$Proportion
+
+dcm_line = 1-(dcm_ben+dcm_lb+dcm_vus_in_hvol)
+
+hcm_single<-ggplot(subset(data2, (Test=='HCM' & Cohort=='HCM')), aes(Cohort, Proportion)) +
+  geom_bar(aes(fill=Class), stat="identity", width=.8, color='black') +
+  scale_fill_manual(values=c("#AA3E39","#D46F6A","grey","#4D658D","#2D4671")) +
+  theme_classic() +
+  theme(legend.position="bottom", legend.title=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank()) +
+  ggtitle("HCM") +
+  coord_flip() +
+  geom_hline(yintercept=hcm_line, linetype = "longdash") + 
+  annotate("segment",x=1.45, xend=1.45, y=hcm_line, yend=(1-(hcm_ben+hcm_lb)), colour="black", size=1, arrow=arrow()) +
+  annotate("text", x=1.452, y=(1-(hcm_ben+hcm_lb)+0.13), label="Background VUS rate") +
+  annotate("segment",x=1.45, xend=1.45, y=hcm_line, yend=(hcm_path+hcm_lp), colour="black", size=1, arrow=arrow()) +
+  annotate("text", x=1.452, y=(hcm_path+hcm_lp-0.11), label="Case VUS excess")
+
+dcm_single<-ggplot(subset(data2, (Test=='DCM' & Cohort=='DCM')), aes(Cohort, Proportion)) +
+  geom_bar(aes(fill=Class), stat="identity", width=.8, color='black') +
+  scale_fill_manual(values=c("#AA3E39","#D46F6A","grey","#4D658D","#2D4671")) +
+  theme_classic() +
+  ggtitle("DCM") +
+  theme(axis.title.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank(), axis.title.y=element_blank(), legend.position="none") +
+  coord_flip() +
+  geom_hline(yintercept=dcm_line, linetype = "longdash") +
+  annotate("segment",x=1.45, xend=1.45, y=dcm_line, yend=(1-(dcm_ben+dcm_lb)), colour="black", size=1, arrow=arrow()) +
+  annotate("text", x=1.452, y=(1-(dcm_ben+dcm_lb)+0.13), label="Background VUS rate")
+
+legend_single <- g_legend(hcm_single)
+hcm_single_no_leg<-hcm_single + theme(legend.position="none")
+
+png("../figures/SingleStackedBars.png",width=1200,height=1000,res=150)
+grid.arrange(hcm_single_no_leg, dcm_single, legend_single, 
+             ncol=1, nrow=3, 
+             layout_matrix=rbind(c(1), c(2), c(3)), 
+             heights=c(10, 10, 1))
+dev.off()
